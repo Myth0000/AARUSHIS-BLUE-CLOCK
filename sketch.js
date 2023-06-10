2820bf296419f6dfbdf2e23036ec220f7ecb03ca
Myth0000 <infinitygovind@gmail.com>
2023-06-10 18:26:16 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,58 @@
+let clock;
+let SalyClock;
+
+function preload(){
+  SalyClock=loadImage("SALYCLOCK.png");
+}
+function setup() {
+  createCanvas(400, 400);
+  clock = {
+    x: 15,
+    y: 17,
+    size:121,
+    
+    
+    Draw() {
+      image(SalyClock, this.x, this.y, this.size, this.size)
+  //rect(this.x,this.y,this.size,this.size);          
+    },
+    rainbow(){
+      this.x=random(0, width-this.size);
+      this.y=random(0, height-this.size);
+    }
+  }
+}
+
+function draw() {
+  background(color(250));
+
+  clock.Draw();
+}
+
+function mouseClicked(){
+  let firstCornerX=clock.x;
+  let firstCornerY=clock.y;
+  let secondCornerX=clock.x+clock.size;
+  let secondCornerY=clock.y;
+  let thirdCornerX=clock.x+clock.size;
+  let thirdCornerY=clock.y+clock.size;
+  let fourthCornerX=clock.x;
+  let fourthCornerY=clock.y+clock.size;
+  
+  if(mouseX>=firstCornerX && mouseX<=secondCornerX &&
+     mouseY>=firstCornerY && mouseY<=fourthCornerY){
+    clock.rainbow();
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
