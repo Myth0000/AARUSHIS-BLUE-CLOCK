30fe592af23270ed17f471bc098285f5f9a0b480
Myth0000 <infinitygovind@gmail.com>
2023-06-10 18:44:58 -0400

    Changed Sizes for stuff

--- sketch.js
+++ sketch.js
@@ -1,26 +1,25 @@
 let clock;
 let SalyClock;
 
-function preload(){
-  SalyClock=loadImage("SALYCLOCK.png");
+function preload() {
+  SalyClock = loadImage("SALYCLOCK.png");
 }
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(950, 1300);
   clock = {
     x: 15,
     y: 17,
-    size:121,
-    
-    
+    size:444,
+
     Draw() {
-      image(SalyClock, this.x, this.y, this.size, this.size)
-  //rect(this.x,this.y,this.size,this.size);          
+      image(SalyClock, this.x, this.y, this.size, this.size);
+      //rect(this.x,this.y,this.size,this.size);
     },
-    rainbow(){
-      this.x=random(0, width-this.size);
-      this.y=random(0, height-this.size);
-    }
-  }
+    rainbow() {
+      this.x = random(0, width - this.size);
+      this.y = random(0, height - this.size);
+    },
+  };
 }
 
 function draw() {
@@ -29,30 +28,22 @@ function draw() {
   clock.Draw();
 }
 
-function mouseClicked(){
-  let firstCornerX=clock.x;
-  let firstCornerY=clock.y;
-  let secondCornerX=clock.x+clock.size;
-  let secondCornerY=clock.y;
-  let thirdCornerX=clock.x+clock.size;
-  let thirdCornerY=clock.y+clock.size;
-  let fourthCornerX=clock.x;
-  let fourthCornerY=clock.y+clock.size;
-  
-  if(mouseX>=firstCornerX && mouseX<=secondCornerX &&
-     mouseY>=firstCornerY && mouseY<=fourthCornerY){
+function mouseClicked() {
+  let firstCornerX = clock.x;
+  let firstCornerY = clock.y;
+  let secondCornerX = clock.x + clock.size;
+  let secondCornerY = clock.y;
+  let thirdCornerX = clock.x + clock.size;
+  let thirdCornerY = clock.y + clock.size;
+  let fourthCornerX = clock.x;
+  let fourthCornerY = clock.y + clock.size;
+
+  if (
+    mouseX >= firstCornerX &&
+    mouseX <= secondCornerX &&
+    mouseY >= firstCornerY &&
+    mouseY <= fourthCornerY
+  ) {
     clock.rainbow();
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 }
